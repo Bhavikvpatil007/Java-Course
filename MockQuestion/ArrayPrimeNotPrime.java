b61cfc3fd79523e0d328ec5be9feb9cd1721b087
Bhavik Patil <163111350+Bhavikvpatil007@users.noreply.github.com>
2026-07-03 23:09:37 +0530

    Update ArrayPrimeNotPrime.java

--- MockQuestion/ArrayPrimeNotPrime.java
+++ MockQuestion/ArrayPrimeNotPrime.java
@@ -1,5 +1,6 @@
+import java.util.Scanner;
 
-public class ArrayPrime {
+public class ArrayPrimeNotPrime {
 
 	public static void main(String[] args) {
 
@@ -17,8 +18,24 @@ public class ArrayPrime {
 
 		for (int i = 0; i < arr.length; i++) {
 
-			if (isPrime(arr[i])) {
-				System.out.print(arr[i] + " ");
+			int num = arr[i];
+			boolean prime = true;
+
+			if (num <= 1) {
+				prime = false;
+			} else {
+
+				for (int j = 2; j < num; j++) {
+					if (num % j == 0) {
+						prime = false;
+						break;
+					}
+				}
+
+			}
+
+			if (prime) {
+				System.out.print(num + " ");
 			}
 
 		}
@@ -29,30 +46,28 @@ public class ArrayPrime {
 
 		for (int i = 0; i < arr.length; i++) {
 
-			if (!isPrime(arr[i])) {
-				System.out.print(arr[i] + " ");
+			int num = arr[i];
+			boolean prime = true;
+
+			if (num <= 1) {
+				prime = false;
+			} else {
+
+				for (int j = 2; j < num; j++) {
+					if (num % j == 0) {
+						prime = false;
+						break;
+					}
+				}
+
+			}
+
+			if (!prime) {
+				System.out.print(num + " ");
 			}
 
 		}
 
+		sc.close();
 	}
-
-	public static boolean isPrime(int num) {
-
-		if (num <= 1) {
-			return false;
-		}
-
-		for (int i = 2; i < num; i++) {
-
-			if (num % i == 0) {
-				return false;
-			}
-
-		}
-
-		return true;
-
-	}
-
 }
