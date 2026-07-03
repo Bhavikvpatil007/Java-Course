236d1e30baebac629cc3f68b44b986304f2bbaee
Bhavik Patil <163111350+Bhavikvpatil007@users.noreply.github.com>
2026-07-03 17:05:23 +0530

    Update ArrayPrimeNotPrime.java
    Prime and Non Prime array From one array

--- MockQuestion/ArrayPrimeNotPrime.java
+++ MockQuestion/ArrayPrimeNotPrime.java
@@ -1,1 +1,58 @@
 
+public class ArrayPrime {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int arr[] = new int[10];
+
+		System.out.println("Enter 10 numbers:");
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		System.out.println("Prime Numbers:");
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if (isPrime(arr[i])) {
+				System.out.print(arr[i] + " ");
+			}
+
+		}
+
+		System.out.println();
+
+		System.out.println("Non Prime Numbers:");
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if (!isPrime(arr[i])) {
+				System.out.print(arr[i] + " ");
+			}
+
+		}
+
+	}
+
+	public static boolean isPrime(int num) {
+
+		if (num <= 1) {
+			return false;
+		}
+
+		for (int i = 2; i < num; i++) {
+
+			if (num % i == 0) {
+				return false;
+			}
+
+		}
+
+		return true;
+
+	}
+
+}
